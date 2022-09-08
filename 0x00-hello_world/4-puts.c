6b66ab16e7937965316516b0dd215bcb22088ce4
NandaDDA <nandadulal2101@gmail.com>
2022-09-08 07:28:13 -0700

    betty coding and c done right

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,17 +3,17 @@
 
 
 /**
- * * main - print the string in the put function
+ *  main - print the string in the put function
  *
- * *
+ * 
  *
- * * Description: using the main function
+ *  Description: using the main function
  *
- * * this program prints "Programming is like building a multilingual puzzle
+ *  this program prints "Programming is like building a multilingual puzzle
  *
- * * Return: 0
+ *  Return: 0
  *
- * */
+ */
 
 int main(void)
 
