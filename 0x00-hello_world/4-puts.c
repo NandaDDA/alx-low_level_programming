88e77c7be37ee9761e556a5377b850a37d85c994
NandaDDA <nandadulal2101@gmail.com>
2022-09-08 08:17:54 -0700

    betty is on my nervees

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - print the string in the put function
  *
- * Description:usingthe main function
- * this program prints "Programming is like building a multilingual puzzle
+ *  main - print the string in the put function
+ *
+ * * Description: using the main function
+ * this program pushs "Programming is like building a multilingual puzzle
  * Return: 0
+ *
  */
 int main(void)
+
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0)
-}		
+
+		printf("/"Programming is like building a multilingual puzzle");
+			return (0);
+}
