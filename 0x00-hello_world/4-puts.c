30b7c8c828a259d5bbfe27be826d23738417cbe1
NandaDDA <nandadulal2101@gmail.com>
2022-09-08 08:53:05 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-
-
 /**
  *  main - Entry point
  *  Description: prints "\"Programming is like building a multilingual puzzle"
@@ -9,9 +7,6 @@
  */    
 int main(void)
 {
-
 		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+           	return (0);
 }
