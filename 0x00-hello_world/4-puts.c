d8acf857e20cd63fb27ec73660c6d9f0dc3066e2
NandaDDA <nandadulal2101@gmail.com>
2022-09-08 08:42:41 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,20 +3,11 @@
 
 
 /**
- *
- *  * main - Entry point
- *
- *   * Description: prints "\"Programming is like building a multilingual puzzle"
- *
- *    * Return: 0
- *
- *
+ *  main - Entry point
+ *  Description: prints "\"Programming is like building a multilingual puzzle"
+ *  Return: 0
  */    
-
-
-
 int main(void)
-
 {
 
 		puts("\"Programming is like building a multilingual puzzle");
