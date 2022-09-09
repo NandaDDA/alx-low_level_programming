4d792f0599269512e4cba75cc470251d6f20f871
NandaDDA <nandadulal2101@gmail.com>
2022-09-09 07:43:07 -0700

    putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints the alphabet in lowercase\n
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char ch:
+for (ch = 'a' ; ch <= 'z' ;ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
