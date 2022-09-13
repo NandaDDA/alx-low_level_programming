72849bbd65e26fcd78fda4808fd95fdf2079fe54
NandaDDA <nandadulal2101@gmail.com>
2022-09-13 12:02:39 -0700

    alfa x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,14 @@
 #include"main.h"
 /**
- * print_alphabet_x10 - function to print abc 10 times
+ * print_alphabet_x10-functiontoprintabc10times
  * Return:0
  */
 void print_alphabet_x10(void)
 {
 char c, i;
-for (i = 0; i <= 9; i++)
+for(i = 0; i <= 9; i++)
 {
-for (c = 'a'; c <= 'z'; c++)
+for(c = 'a'; c <= 'z'; c++)
 {	
 _putchar(c);
 }
