e95c6bebd597f26fed921c1db80b7fb5080baa18
NandaDDA <nandadulal2101@gmail.com>
2022-09-22 05:17:28 -0700

    vi 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,19 +12,19 @@ while (str[++index])
 while (!(str[index] >= 'a' && str[index] <= 'z'))
 index++;
 if (str[index - 1] == ' ' ||
-str [index - 1] == '\t' ||
-str [index - 1] == '\n' ||
-str [index - 1] == ',' ||
-str [index - 1] == ';' ||
-str [index - 1] == '.' ||
-str [index - 1] == '!' ||
-str [index - 1] == '?' ||
-str [index - 1] == '"' ||
-str [index - 1] == '(' ||
-str [index - 1] == ')' ||
-str [index - 1] == '{' ||
-str [index - 1] == '}')
-str [index] -= 32;
+str[index - 1] == '\t' ||
+str[index - 1] == '\n' ||
+str[index - 1] == ',' ||
+str[index - 1] == ';' ||
+str[index - 1] == '.' ||
+str[index - 1] == '!' ||
+str[index - 1] == '?' ||
+str[index - 1] == '"' ||
+str[index - 1] == '(' ||
+str[index - 1] == ')' ||
+str[index - 1] == '{' ||
+str[index - 1] == '}')
+str[index] -= 32;
 }
 return (str);
 }
