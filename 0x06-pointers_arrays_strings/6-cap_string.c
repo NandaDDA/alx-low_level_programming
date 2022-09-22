de050259e388112a4958a938c8a6a17a16739759
NandaDDA <nandadulal2101@gmail.com>
2022-09-22 05:14:58 -0700

    vi 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -4,7 +4,7 @@
  *@str:stringtocapitalize
  *Return:addressofthecapitalizedstring
  */
-char*cap_string(char *str)
+char *cap_string(char *str)
 {
 int index = 0;
 while (str[++index])
@@ -12,19 +12,19 @@ while (str[++index])
 while (!(str[index] >= 'a' && str[index] <= 'z'))
 index++;
 if (str[index - 1] == ' ' ||
-str[index - 1] == '\t' ||
-str[index - 1] == '\n' ||
-str[index - 1] == ',' ||
-str[index - 1] == ';' ||
-str[index - 1] == '.' ||
-str[index - 1] == '!' ||
-str[index - 1] == '?' ||
-str[index - 1] == '"' ||
-str[index - 1] == '(' ||
-str[index - 1] == ')' ||
-str[index - 1] == '{' ||
-str[index - 1] == '}')
-str[index] -= 32;
+str [index - 1] == '\t' ||
+str [index - 1] == '\n' ||
+str [index - 1] == ',' ||
+str [index - 1] == ';' ||
+str [index - 1] == '.' ||
+str [index - 1] == '!' ||
+str [index - 1] == '?' ||
+str [index - 1] == '"' ||
+str [index - 1] == '(' ||
+str [index - 1] == ')' ||
+str [index - 1] == '{' ||
+str [index - 1] == '}')
+str [index] -= 32;
 }
 return (str);
 }
