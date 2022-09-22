c54397380dd283fcb1ac599db2584815bd6b8a85
NandaDDA <nandadulal2101@gmail.com>
2022-09-22 05:11:02 -0700

    vi 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,22 +1,10 @@
 #include "main.h"
 /**
- * _strlen-returns the length of a string
- * @s: string
- * Return: returns length as integer;
- */
-int _strlen(char *s)
-{
-int len = 0;
-while (*(s + len) !='\n')
-len++;
-return (len);
-}
-/**
  *cap_string-capitalizesallwordsinastring
  *@str:stringtocapitalize
- *Return: address of the capitalized string
+ *Return:addressofthecapitalizedstring
  */
-char *cap_string(char *str)
+char*cap_string(char *str)
 {
 int index = 0;
 while (str[++index])
