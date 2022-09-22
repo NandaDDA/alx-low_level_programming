6ea5060caae955bbc10b54c07564a751f15eb44c
NandaDDA <nandadulal2101@gmail.com>
2022-09-22 06:27:03 -0700

    return (str);

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,24 +6,26 @@
  */
 char *cap_string(char *str)
 {
-int i = 0, j;
-char a[] = " \t\n,;.!?\"(){}";
-while (*(s + i))
+int index = 0;
+while (str[++index])
 {
-if (*(s + i) >= 'a' && *(s + i) <= 'z')
-{
-if (i == 0)
-*(s + i) -= 'a' - 'A';
-else
-{
-for (j = 0; j <= 12; j++)
-{
-if (a[j] == *(s + i - 1))
-*(s + i) -= 'a' - 'A';
-}        
+while (!(str[index] >= 'a' && str[index] <= 'z'))	
+index++;
+
+if (str[index - 1] == ' ' ||
+		str[index - 1] == '\t' ||
+		str[index - 1] == '\n' ||
+		str[index - 1] == ',' ||
+		str[index - 1] == ';' ||
+		str[index - 1] == '.' ||
+		str[index - 1] == '!' ||
+		str[index - 1] == '?' ||
+		str[index - 1] == '"' ||
+		str[index - 1] == '(' ||
+		str[index - 1] == ')' ||
+		str[index - 1] == '{' ||
+		str[index - 1] == '}')
+	str[index] -= 32;
 }
-}
-i++;
-}
-return (s);
+return (str);
 }
