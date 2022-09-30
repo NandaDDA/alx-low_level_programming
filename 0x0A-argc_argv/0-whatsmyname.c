325c7501cad448f2ab8bab049ca9a6041fb0f0d8
NandaDDA <nandadulal2101@gmail.com>
2022-09-30 05:31:37 -0700

    you know

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 /**
  * main - prints program name
@@ -7,6 +8,6 @@
  */
 int main(int __attribute argc, char *argv[])
 {
-printf("%s\n", argv[0]);
+_putchar("%s\n", argv[0]);
 return (0);
 }
