ad31df23d4fe6cf2bfc6234411c3eb7258d84958
NandaDDA <nandadulal2101@gmail.com>
2022-09-30 04:29:18 -0700

    amended

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+/**
+ * int prototypes()
+ */
+int _putchar(char c)
+int main(int argc, char __attribute__((__unused__)) *argv[])
+int main(int argc, char *argv[])
+
+#endif /*MAIN_H*/
