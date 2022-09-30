9b402e5607e5a2ded92ea510c3694d6720bf31aa
NandaDDA <nandadulal2101@gmail.com>
2022-09-30 06:25:27 -0700

    of two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - prints multiplication of two numbers
+ * main - prints multiplication
+ * of two numbers
  * @argc: number of arguments
  * @argv: array of arguments
  * Return: returns 0
