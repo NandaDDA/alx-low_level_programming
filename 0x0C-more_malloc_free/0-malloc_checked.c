e4f771c49345f441cfc3dd8276b74ddaa70af4c7
NandaDDA <nandadulal2101@gmail.com>
2022-10-06 03:10:56 -0700

    should work

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 /**
  * malloc_checked - allocates memory using malloc, exit(98) if it fails
  * @b: size of the memory block to be allocated
